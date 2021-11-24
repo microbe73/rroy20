f424a41a6cd7c78e8d549e9dfe0473bf34430259
rit roy <ritroy@rits-MacBook-Air.local>
2021-11-23 22:07:04 -0500

    added css assignment 18 and api assignment 19

---
+++ 19_api/app.py
@@ -0,0 +1,20 @@
+import urllib
+from flask import Flask,render_template
+import json
+app = Flask(__name__) 
+@app.route('/')
+def display():
+    req = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=pJkgjfYApMvTrcMw0O7hdO7Chooqu9b4jcuDu2zC')
+    text = json.load(req)
+    #for i in text:
+        #print(json.load(r)[i])
+    picture = (text['url'])
+    description = (text['explanation'])
+    return render_template('main.html', pic = picture, desc = description)
+        
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
