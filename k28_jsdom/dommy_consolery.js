ccf906d5ba981ec71ec30e1c0a860201d6efe573
Rayat Roy <57008685+microbe73@users.noreply.github.com>
2022-02-09 20:08:41 -0500

    Add files via upload

---
+++ k28_jsdom/dommy_consolery.js
@@ -0,0 +1,133 @@
+/*
+   your PPTASK:
+   
+   Test drive each bit of code in this file,
+    and insert comments galore, indicating anything
+     you discover,
+    	have questions about,
+    		or otherwise deem notable.
+    		
+    		Write with your future self or teammates in mind.
+    		
+    		If you find yourself falling out of flow mode, consult 
+    		other teams
+    		MDN
+
+   A few comments have been pre-filled for you...
+   
+   (delete this block comment once you are done)
+*/
+
+// Team Phantom Tollbooth :: Rayat Roy, Sophie Liu
+// SoftDev pd0
+// K28 -- Getting more comfortable with the dev console and the DOM
+// 2022-02-08t
+// --------------------------------------------------
+
+
+//send diagnostic output to console
+//(Ctrl-Shift-K in Firefox to reveal console)
+console.log("AYO");
+
+var i = "hello";
+var j = 20;
+
+
+//assign an anonymous fxn to a var
+var f = function(x) {
+  var j=30;
+  return j+x;
+};
+
+
+//instantiate an object
+var o = { 'name' : 'Thluffy',
+          age : 15,
+          items : [10, 20, 30, 40],
+          morestuff : {a : 1, b : 'ayo'},
+          func : function(x) {
+            return x+30;
+          }
+        };
+
+
+var addItem = function(text) {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = text;
+  list.appendChild(newitem);
+};
+
+
+var removeItem = function(n) {
+  var listitems = document.getElementsByTagName('li');
+  listitems[n].remove();
+};
+
+
+var red = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    items[i].classList.add('red');
+  }
+};
+
+
+var stripe = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    if (i%2==0){
+      items[i].classList.add('red');
+    } else {
+      items[i].classList.add('blue');
+    }
+  }
+};
+/*
+document.getElementById(<ID>)
+document.getElementsByClassName(<class>)
+<button id="b">
+var a = document.getElementById("b")
+a.addEventListener(<EVENT>,<FUNCTION>)
+'click',fxnname
+document.createElement(<HTML TAG NAME>)
+element.setAttribute(<NAME>,<VALUE>)
+element.getAttribute(<NAME>)
+*/
+var fxn = function() {
+  let c = document.createElement('p');
+  let a = fib(5);
+  c.innerHTML = '50';
+  return c;
+}
+var fib = (n) => {
+  if(n == 1){
+    return 1;
+  };
+  if(n == 0){
+    return 0;
+  };
+  return fib(n-1)+fib(n-2);
+};
+var fact = (n) => {
+  if(n==0){
+    return 1;
+  };
+  return n * fact(n-1);
+};
+
+
+var gcd = (a,b) => {
+  if(a > b){
+    if(a % b === 0){
+      return b;
+    }
+    return gcd(b, a % b);
+  }
+  if(b > a){
+    if(b % a === 0){
+      return a;
+    }
+    return gcd(a, b % a);
+  }
+}
